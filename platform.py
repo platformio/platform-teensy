b416f637a1b2f9b5e6fc427163aa9d5165410e90
Ivan Kravets <me@ikravets.com>
2018-07-17 20:20:58 +0300

    Use J-Link package from PlatformIO Registry

--- platform.py
+++ platform.py
@@ -32,6 +32,21 @@ class TeensyPlatform(PlatformBase):
             self.packages["toolchain-gccarmnoneeabi"][
                 'version'] = ">=1.60301.0"
 
+        # configure J-LINK tool
+        jlink_conds = [
+            "jlink" in variables.get(option, "")
+            for option in ("upload_protocol", "debug_tool")
+        ]
+        if variables.get("board"):
+            board_config = self.board_config(variables.get("board"))
+            jlink_conds.extend([
+                "jlink" in board_config.get(key, "")
+                for key in ("debug.default_tools", "upload.protocol")
+            ])
+        jlink_pkgname = "tool-jlink"
+        if not any(jlink_conds) and jlink_pkgname in self.packages:
+            del self.packages[jlink_pkgname]
+
         return PlatformBase.configure_default_packages(
             self, variables, targets)
 
@@ -58,6 +73,7 @@ class TeensyPlatform(PlatformBase):
                 "Missed J-Link Device ID for %s" % board.id)
             debug['tools']['jlink'] = {
                 "server": {
+                    "package": "tool-jlink",
                     "arguments": [
                         "-singlerun",
                         "-if", "SWD",
